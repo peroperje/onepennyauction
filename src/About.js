f329b45d9df451c0fc42aa87427549a0d97a8d6b
peroperje <el.pionir@gmail.com>
2017-02-10 10:35:02 +0100

    update redame

--- src/About.js
+++ src/About.js
@@ -7,7 +7,6 @@ import React, {Component} from 'react';
 
 class About extends Component {
     render(){
-        console.log(ReadME);
         return (
             <div>
                 <h1>About</h1>
